72249a02bc87cd3d22711f67624c56c9aae12700
Ajah Emmanuel <emmanuel.fmrs.marach@gmail.com>
2024-02-01 21:09:51 +0100

    Worked on the backend functions. Started the todo home view

--- task1_todo_list_app/lib/bloc/app_backend.dart
+++ task1_todo_list_app/lib/bloc/app_backend.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 
@@ -20,11 +24,33 @@ class AppBackend {
     return pref.getString(username);
   }
 
-  Future<String?> pickImage() async{
+  Future<File> pickImage() async{
     final imagePicker = ImagePicker();
     final file = await imagePicker.pickImage(
       source: ImageSource.gallery
     );
-    return await file?.readAsString();
+    return File(file!.path);
+  }
+
+  Future<void> saveToLocalDirectory(File file) async{
+    final prefs = await preferences;
+    final deviceDirectory = await getApplicationDocumentsDirectory();
+    final fileExtension = file.path.split('.').last;
+    final newFilePath = join(deviceDirectory.path, 'dp.$fileExtension');
+    prefs.setString('newFilePath', newFilePath);
+    await file.copy(newFilePath);
+  }
+
+  Future<Uint8List?> retrieveFromLocalDirectory() async{
+    final prefs = await preferences;
+    final newFilePath = prefs.getString('newFilePath');
+    if(newFilePath != null){
+      final newFile = File(newFilePath);
+      if(await newFile.exists()){
+        final imageBytes = await newFile.readAsBytes();
+        return imageBytes;
+      }
+    }
+    return null;
   }
 }
